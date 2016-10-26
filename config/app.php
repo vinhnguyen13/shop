2b7e69c4f57b3e77562910b14ab80b0fe9a34d3b
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-10-26 17:59:35 +0700

    install

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /*
+         * IDE helper
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
