6a32a8990ff7f5c4a0cb91b5a3665bf728d4dd82
VinhNguyen <quangvinhit2007@gmail.com>
2017-01-17 23:50:22 +0700

    shop payment

--- resources/themes/v1/views/product/partials/items.blade.php
+++ resources/themes/v1/views/product/partials/items.blade.php
@@ -10,27 +10,27 @@
                     <div class="product__img">
                         <img data-original="{{$product->thumb()}}" alt="" class="lazy" />
                     </div>
-                    <div class="product__item--infor">
-                        <div class="text-center mgB-40">
-                            @if (!empty($sizes) && count($sizes) > 0)
-                                <div class="size-item">
-                                    @foreach($sizes as $size)
-                                        <span class="size">{{$size->size}}</span>
-                                    @endforeach
-                                </div>
-                                <p class="text-uper">available size</p>
-                            @else
-                                <div class="size-item">&nbsp;</div>
-                                <p class="text-uper">out of stock</p>
-                            @endif
-                        </div>
-                        <div class="clearfix">
-                            <p class="fs-18 fontSFUBold pull-right">{{number_format($product->price, 0)}}</p>
-                            <p class="product-name text-uper">{{$product->name}}</p>
-                            <p class="product-type text-uper">{{$product->color}}</p>
-                        </div>
-                    </div>
                 </a>
+                <div class="product__item--infor">
+                    <div class="text-center mgB-20">
+                        @if (!empty($sizes) && count($sizes) > 0)
+                            <div class="size-item">
+                                @foreach($sizes as $size)
+                                    <a href="" class="size">{{$size->size}}</a>
+                                @endforeach
+                            </div>
+                            <p class="text-uper">available size</p>
+                        @else
+                            <div class="size-item">&nbsp;</div>
+                            <p class="text-uper">out of stock</p>
+                        @endif
+                    </div>
+                    <div class="clearfix">
+                        <p class="fs-18 fontSFUBold pull-right">{{number_format($product->price, 0)}}</p>
+                        <p class="product-name text-uper">{{$product->name}}</p>
+                        <p class="product-type text-uper">{{$product->color}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
