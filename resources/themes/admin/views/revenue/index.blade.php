7e2a9fae497898463de56c00f8caf950048af85c
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2017-03-01 19:07:53 +0700

    shop frontend

--- resources/themes/admin/views/revenue/index.blade.php
+++ resources/themes/admin/views/revenue/index.blade.php
@@ -43,6 +43,28 @@
         <div class="box-footer clearfix">
         </div>
     </div>
+
+    <div class="modal fade" id="modal-qrcode" data-backdrop="static">
+        <div class="modal-dialog" style="width: 350px;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"><i class="fa fa-book"></i> Staff </h4>
+                </div>
+                <div class="modal-body">
+                    <p class="hint">
+                        Đây là phần thanh toán tiền cho supplier.<br/>
+                        Vì vậy chúng tôi cần xác thực bạn có phải là nhân viên của chúng tôi không ?
+                    </p>
+                    <input type="text" class="form-control date" placeholder="Email" name="to_date">
+                    <input type="text" class="form-control date" placeholder="Password" name="to_date">
+                </div>
+                <div class="modal-footer">
+                    <button id="btn-print" type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-check"></i> Sign In</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('styles')
@@ -67,6 +89,7 @@
         });
 
         $('.wrapRevenue').on('click', '.btn-payment-supplier', function (e) {
+            $('#modal-qrcode').modal('show');
             return false;
         });
 
