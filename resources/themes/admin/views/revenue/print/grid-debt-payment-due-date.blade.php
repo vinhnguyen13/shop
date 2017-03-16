f2c4c3c45edcbe3311dcc305ee8fd27845849dbc
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2017-03-16 15:25:53 +0700

    Revenue print

--- resources/themes/admin/views/revenue/print/grid-debt-payment-due-date.blade.php
+++ resources/themes/admin/views/revenue/print/grid-debt-payment-due-date.blade.php
@@ -1,63 +1,106 @@
 @extends('layouts.print')
 
 @section('content')
-    @if(!empty($orders))
-        <?php
-        $paymentTotal = 0;
-        $revenueTotal = 0;
-        $consignmentPaymentTotal = 0;
-        ?>
-        <table class="table table-bordered">
-            <tbody><tr>
-                <th style="width: 10px">ID</th>
-                <th>Date</th>
-                <th>SKU</th>
-                <th>Supplier</th>
-                <th>Product</th>
-                <th width="2%">Size</th>
-                <th width="2%">QTY</th>
-                <th>Price</th>
-                <th>Payment</th>
-                <th>Consignment Payment</th>
-            </tr>
-            @foreach($orders as $order)
-                <?php
-                $date = date('d-m-Y', strtotime($order->created_at));
-                $supplierHtml = '<a href="'.route('admin.supplier.index', ['id'=>$order->supplier_id]).'">'.$order->supplier->name.'</a><br>';
-                $supplierHtml .= '<p class="help-block small">Discount: '.number_format($order->supplier->discount_available).' %</p>';
-                $priceHtml = number_format($order->price).'<br/>';
-                $priceHtml .= '<p class="help-block small">Price In: '.number_format($order->price_in).'</p>';
-                $revenue = $order->total * $order->supplier->discount_available / 100;
-                $revenueHtml = number_format($revenue);
-                $consignmentPayment = $order->total - $revenue;
-                $consignmentPaymentHtml = number_format($consignmentPayment);
-                $consignmentPaymentHtml .= '<p class="help-block small">Payment Date: '.date('d-m-Y', strtotime('+'.\App\Models\ShopProductDetail::DUE_DAYS.' days', strtotime($order->created_at))).'</p>';
-                $paymentTotal += $order->total;
-                $revenueTotal += $revenue;
-                $consignmentPaymentTotal += $consignmentPayment;
-                ?>
-                <tr>
-                    <td>{{$order->id}}</td>
-                    <td>{{$date}}</td>
-                    <td><a href="{{route('admin.product-detail.index', ['id'=>$order->product_detail_id])}}">{{$order->sku}}</a></td>
-                    <td>{!! $supplierHtml !!}</td>
-                    <td>{{$order->product_name}}</td>
-                    <td>{{$order->size}}</td>
-                    <td>{{$order->quantity}}</td>
-                    <td>{!! $priceHtml !!}</td>
-                    <td>{{number_format($order->total)}}</td>
-                    <td>{!! $consignmentPaymentHtml !!}</td>
-                </tr>
-            @endforeach
-            <tr>
-                <td colspan="8" class="text-right text-bold">Total</td>
-                <td class="text-bold">{{number_format($paymentTotal)}}</td>
-                <td class="text-bold">{{number_format($consignmentPaymentTotal)}}</td>
-            </tr>
-            </tbody>
-        </table>
-        <div class="pagination pagination-sm no-margin pull-right">
-            {{ $orders->links('vendor.pagination.default') }}
+    <div class="print__a5">
+        <div class="clearfix mgB-15">
+            <div class="pull-left">
+                <p class="fontSFUMeBold fs-17 lh-50 mgT-10">PAYMENT NO #:</p>
+            </div>
+            <div class="pull-right text-right">
+                <div class="mgB-10"><img src="icons/logo-footer.svg" alt="" width="80"></div>
+                <p class="fontSFUMeBold fs-14">135/58 Trần Hưng Đạo , District 1, HCM city , Việt Nam</p>
+                <p class="fontSFUMeBold fs-14">glab.vn@gmail.com  -   094 537 88 09</p>
+            </div>
         </div>
-    @endif
+        <p class="fontSFURe text-center fs-17 mgB-10 font-600">CUSTOMER INFO</p>
+        <div class="mgB-5">
+            <span class="d-ib w-32 fontSFUMeBold">DATE:</span>
+            <span class="d-ib w-32 fontSFUMeBold">NAME:</span>
+            <span class="d-ib w-32 fontSFUMeBold">CMND:</span>
+        </div>
+        <div class="mgB-5">
+            <span class="d-ib w-32 fontSFUMeBold">TIME:</span>
+            <span class="d-ib w-32 fontSFUMeBold">EMAIL:</span>
+            <span class="d-ib w-32 fontSFUMeBold">ADDRESS:</span>
+        </div>
+        <div class="mgB-15">
+            <span class="d-ib w-32 fontSFUMeBold">STAFF:</span>
+            <span class="d-ib w-32 fontSFUMeBold">PHONE:</span>
+        </div>
+        @if(!empty($orders))
+            <?php
+            $paymentTotal = 0;
+            $revenueTotal = 0;
+            $consignmentPaymentTotal = 0;
+            $num = 1;
+            ?>
+            <table>
+                <thead><tr>
+                    <th class="w-5 fontSFURe text-center font-600">ID</th>
+                    <th class="w-15">
+                        <p class="fontSFURe font-600">SALEDATE</p>
+                        <p class="fontSFURe color-7c7c7c fs-11">(IMPORT DATE)</p>
+                    </th>
+                    <td class="w-40"><p class="fontSFURe font-600">PRODUCT </p></td>
+                    <td class="w-5 text-center"><p class="fontSFURe font-600">SIZE</p></td>
+                    <td class="w-5 text-center"><p class="fontSFURe font-600">QTY</p></td>
+                    <td class="text-right w-25">
+                        <p class="fontSFURe font-600">CONSIGNMENT PAYMENT</p>
+                        <p class="fontSFURe color-7c7c7c fs-11">PRICE</p>
+                    </td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($orders as $order)
+                    <?php
+                    $date1 = date('d-m-Y', strtotime($order->created_at));
+                    $date2 = date('d-m-Y', strtotime('+'.\App\Models\ShopProductDetail::DUE_DAYS.' days', strtotime($order->created_at)));
+                    $revenue = $order->total * $order->supplier->discount_available / 100;
+                    $consignmentPayment = $order->total - $revenue;
+                    $consignment1 = number_format($consignmentPayment);
+                    $consignment2 = number_format($order->price);
+                    $paymentTotal += $order->total;
+                    $revenueTotal += $revenue;
+                    $consignmentPaymentTotal += $consignmentPayment;
+                    ?>
+                    <tr>
+                        <td class="fontSFURe text-center font-600"><p>{{$num}}</p></td>
+                        <td>
+                            <p class="fontSFUMeBold font-600 fs-12">{{$date1}}</p>
+                            <p class="fontSFUMeBold color-7c7c7c fs-11">{{$date2}}</p>
+                        </td>
+                        <td>
+                            <p class="fontSFUMeBold">{{$order->product_name}}  (SKU : {{$order->sku}})</p>
+                        </td>
+                        <td class="text-center"><p class="fontSFUMeBold">{{$order->size}}</p></td>
+                        <td class="text-center"><p class="fontSFUMeBold">{{$order->quantity}}</p></td>
+                        <td class="text-right">
+                            <p class="fontSFUMeBold">{{$consignment1}}</p>
+                            <p class="fontSFUMeBold color-7c7c7c fs-11">{{$consignment2}}</p>
+                        </td>
+                    </tr>
+                    <?php
+                    $num++;
+                    ?>
+                @endforeach
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-center"><p class="fontSFURe font-600 fs-16">TOTAL</p></td>
+                    <td class="text-right"><p class="fontSFURe font-600 fs-16">{{number_format($consignmentPaymentTotal)}} đ</p></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><p class="fontSFUMeBold font-600 fs-15">G-LAB</p></td>
+                    <td><p class="fontSFUMeBold font-600 fs-15">CUSTOMER</p></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        @endif
+    </div>
 @endsection
--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.blank')
-
-@section('content')
-    @yield('content')
-@endsection
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+    <title>{{ config('app.name', 'Glab') }}</title>
+    <link rel="stylesheet" href="{{asset('/themes/v1/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/themes/v1/css/reset.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/themes/v1/css/font.css')}}">
+    <link rel="stylesheet" href="{{asset('/themes/v1/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('/themes/v1/css/responsive.css')}}">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
