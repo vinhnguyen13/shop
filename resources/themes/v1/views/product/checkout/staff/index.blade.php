f9e480a95f586a3b075f1099c5286f462e0008a6
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2017-06-15 18:48:20 +0700

    checkout for staff

--- resources/themes/v1/views/product/checkout/staff/index.blade.php
+++ resources/themes/v1/views/product/checkout/staff/index.blade.php
@@ -10,124 +10,124 @@
             </ul>
         </div>
         <div class="checkout__inner clearfix">
-            <div class="checkout__infor" style="width:100%;">
-                <div class="filter__product">
-                    <div class="frm-item">
-                        <div class="frm-item-icon searchProduct" data-url-products="{{route('api.product.search')}}" data-url-product="{{route('api.product.get')}}">
-                            <input type="text" placeholder="Name , SKU , Color ...">
-                            <button><span class="icon-slice9"></span></button>
+            <form class="frm-filter">
+                <div class="checkout__infor" style="width:100%;">
+                    <div class="filter__product">
+                        <div class="frm-item">
+                            <div class="frm-item-icon searchProduct" data-url-products="{{route('api.product.search')}}" data-url-product="{{route('api.product.get')}}">
+                                <input type="text" placeholder="Name , SKU , Color ..." name="word">
+                                <button><span class="icon-slice9"></span></button>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="filter__item">
-                        <form class="frm-filter">
-                        <table>
-                            <tr>
-                                <td>
-                                    <div class="dropdown">
-                                        <a href="" class="val-selected">
-                                            Brand
-                                            <div class="get-val"></div>
-                                            <span class="icon-uniF140"></span>
-                                        </a>
-                                        <div class="dropdown-up-style hide">
-                                            <div class="dropdown__inner">
-                                                @if (!empty($manufacturers))
-                                                <ul>
-                                                    <li><a href="" data-value="0">- None -</a></li>
-                                                    @foreach($manufacturers as $manufacturer)
-                                                    <li><a href="" data-value="{{$manufacturer->id}}">{{$manufacturer->name}}</a></li>
-                                                    @endforeach
-                                                </ul>
-                                                @endif
+                        <div class="filter__item">
+                            <table>
+                                <tr>
+                                    <td>
+                                        <div class="dropdown">
+                                            <a href="" class="val-selected">
+                                                Brand
+                                                <div class="get-val"></div>
+                                                <span class="icon-uniF140"></span>
+                                            </a>
+                                            <div class="dropdown-up-style hide">
+                                                <div class="dropdown__inner">
+                                                    @if (!empty($manufacturers))
+                                                    <ul>
+                                                        <li><a href="" data-value="0">- None -</a></li>
+                                                        @foreach($manufacturers as $manufacturer)
+                                                        <li><a href="" data-value="{{$manufacturer->id}}">{{$manufacturer->name}}</a></li>
+                                                        @endforeach
+                                                    </ul>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                            <input type="hidden" value="" name="manufacturer" />
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <a href="" class="val-selected">
+                                                Size
+                                                <div class="get-val"></div>
+                                                <span class="icon-uniF140"></span>
+                                            </a>
+                                            <div class="dropdown-up-style hide">
+                                                <div class="dropdown__inner">
+                                                    @if (!empty($sizes))
+                                                    <ul>
+                                                        {{--<li><label for="" class="frm"><input type="radio" />Men US</label></li>--}}
+                                                        {{--<li><label for="" class="frm"><input type="radio" />Women US</label></li>--}}
+                                                        @foreach($sizes as $size)
+                                                            <li><a href="" data-value="{{$size}}">{{$size}}</a></li>
+                                                        @endforeach
+                                                    </ul>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                            <input type="hidden" value="" name="size" />
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <a href="" class="val-selected">
+                                                Color
+                                                <div class="get-val"></div>
+                                                <span class="icon-uniF140"></span>
+                                            </a>
+                                            <div class="dropdown-up-style hide">
+                                                <div class="dropdown__inner">
+                                                    <ul>
+                                                        <li><a href="" data-value="1">red</a></li>
+                                                        <li><a href="" data-value="2">black</a></li>
+                                                        <li><a href="" data-value="3">bla bla</a></li>
+                                                        <li><a href="" data-value="4">bla bla</a></li>
+                                                        <li><a href="" data-value="5">bla bla</a></li>
+                                                        <li><a href="" data-value="6">bla bla</a></li>
+                                                        <li><a href="" data-value="7">bla bla</a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <input type="hidden" value="" name="color" />
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="clearfix price__range">
+                                            <div class="pull-left">
+                                                <p class="font-600">From</p>
+                                            </div>
+                                            <div class="pull-right">
+                                                <input type="text" value="" class="filter__price" name="from_price" />đ
                                             </div>
                                         </div>
-                                        <input type="hidden" value="" name="manufacturer" />
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="dropdown">
-                                        <a href="" class="val-selected">
-                                            Size
-                                            <div class="get-val"></div>
-                                            <span class="icon-uniF140"></span>
-                                        </a>
-                                        <div class="dropdown-up-style hide">
-                                            <div class="dropdown__inner">
-                                                @if (!empty($sizes))
-                                                <ul>
-                                                    {{--<li><label for="" class="frm"><input type="radio" />Men US</label></li>--}}
-                                                    {{--<li><label for="" class="frm"><input type="radio" />Women US</label></li>--}}
-                                                    @foreach($sizes as $size)
-                                                        <li><a href="" data-value="{{$size}}">{{$size}}</a></li>
-                                                    @endforeach
-                                                </ul>
-                                                @endif
+                                    </td>
+                                    <td>
+                                        <div class="clearfix price__range">
+                                            <div class="pull-left">
+                                                <p class="font-600">To</p>
+                                            </div>
+                                            <div class="pull-right pdR-10">
+                                                <input type="text" value="" class="filter__price" name="to_price" />đ
                                             </div>
                                         </div>
-                                        <input type="hidden" value="" name="size" />
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="dropdown">
-                                        <a href="" class="val-selected">
-                                            Color
-                                            <div class="get-val"></div>
-                                            <span class="icon-uniF140"></span>
-                                        </a>
-                                        <div class="dropdown-up-style hide">
-                                            <div class="dropdown__inner">
-                                                <ul>
-                                                    <li><a href="" data-value="1">red</a></li>
-                                                    <li><a href="" data-value="2">black</a></li>
-                                                    <li><a href="" data-value="3">bla bla</a></li>
-                                                    <li><a href="" data-value="4">bla bla</a></li>
-                                                    <li><a href="" data-value="5">bla bla</a></li>
-                                                    <li><a href="" data-value="6">bla bla</a></li>
-                                                    <li><a href="" data-value="7">bla bla</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <input type="hidden" value="" name="color" />
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="clearfix price__range">
-                                        <div class="pull-left">
-                                            <p class="font-600">From</p>
-                                        </div>
-                                        <div class="pull-right">
-                                            <input type="text" value="" class="filter__price" name="from_price" />đ
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="clearfix price__range">
-                                        <div class="pull-left">
-                                            <p class="font-600">To</p>
-                                        </div>
-                                        <div class="pull-right pdR-10">
-                                            <input type="text" value="" class="filter__price" name="to_price" />đ
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        </table>
-                        <div class="filter__item--btn text-center">
-                            <button class="btn-small btn-apply">APPLY</button>
-                            <button class="btn-reset">RESET</button>
+                                    </td>
+                                </tr>
+                            </table>
+                            <div class="filter__item--btn text-center">
+                                <button class="btn-small btn-apply">APPLY</button>
+                                <button class="btn-reset">RESET</button>
+                            </div>
                         </div>
-                        </form>
-                    </div>
 
-                    <div class="filter__result">
-                        <div class="alert alert-info fade in alert-dismissable">
-                            <h4><i class="icon fa fa-warning"></i> Thông báo!</h4>
-                            Bạn vui lòng nhập điều kiện để lọc sản phẩm cần tìm
+                        <div class="filter__result">
+                            <div class="alert alert-info fade in alert-dismissable">
+                                <h4><i class="icon fa fa-warning"></i> Thông báo!</h4>
+                                Bạn vui lòng nhập điều kiện để lọc sản phẩm cần tìm
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 @endsection
