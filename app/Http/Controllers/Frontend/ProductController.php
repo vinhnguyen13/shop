9ead16a541a563dc094be0283cbd2d355fab32ab
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2017-10-04 14:34:47 +0700

    Management Size

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -176,34 +176,14 @@ class ProductController extends Controller
             $from_price = $request->get('from_price');
             $to_price = $request->get('to_price');
             $cart = app(Payment::class)->getCart();
-            $query = ShopProductDetail::query()
-                ->join('shop_product', function($query) use ($manufacturer, $size, $color) {
-                    //link customer to their orders
-                    $query->on('shop_product_detail.product_id', '=', 'shop_product.id');
-                    //consider only enabled orders
-                    if(!empty($manufacturer)) {
-                        $query->where('shop_product.manufacturer_id', '=', $manufacturer);
-                    }
-                    if(!empty($size)){
-                        $query->where(['shop_product_detail.size'=>$size]);
-                    }
-                    if(!empty($color)){
-                        $query->where(['shop_product_detail.color'=>$color]);
-                    }
-                    //consider only orders where status != 0
-                })
-                ->where(['shop_product_detail.stock_status_id' => ShopProductDetail::STOCK_IN_STOCK]);
-            if(!empty($from_price) || !empty($to_price)){
-                $query->whereBetween('price', [$from_price, $to_price]);
-            }
-            if(!empty($word)){
-                $query->where(function($query) use ($word){
-                    $query->where('name', 'like', '%'.$word.'%');
-                    $query->orWhere('shop_product.color', 'like', '%'.$word.'%');
-                    $query->orWhere('shop_product.sku_producer', 'like', '%'.$word.'%');
-                });
-            }
-            $details = $query->groupBy('shop_product.id')->paginate(20);
+            $details = app(ShopProductDetail::class)->findProductByStaff([
+                'word'=>$word,
+                'manufacturer'=>$manufacturer,
+                'size'=>$size,
+                'color'=>$color,
+                'from_price'=>$from_price,
+                'to_price'=>$to_price,
+            ]);
             return view('product.checkout.staff.filter-result', compact('sizes', 'details', 'cart'));
         }
         return view('product.checkout.staff.index', compact('size', 'sizes'));
