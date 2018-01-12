4add2869205686f32c9b283f882271f766292649
DOMAIN1\vinh.nguyen2 <vinh.nguyen2@persol.co.jp>
2018-01-12 16:57:49 +0700

    Fix some bug

--- resources/themes/v1/views/page/print-invoice.blade.php
+++ resources/themes/v1/views/page/print-invoice.blade.php
@@ -3,14 +3,10 @@
 @section('content')
     <div class="reciep">
         <div class="reciep__top">
-            <a href="{{url('/')}}" class="reciep__logo"><img src="/themes/v1/icons/logo-footer.svg" alt=""></a>
-            <p class="font-500 fs-13 mgB-5 font-600">135/58 Trần Hưng Đạo , District 1, HCM city , Việt Nam.</p>
-            <p class="font-500 fs-13 mgB-15 font-600">glab.vn@gmail.com  -   094 537 88 09</p>
-            <p class="font-500 fs-13 font-600">Sản phẩm chỉ được đổi trả trong vòng 03 ngày kể từ ngày mua hàng với điều kiện quý khách còn giữ hóa đơn và sản phẩm chưa qua sử dụng còn nguyên nhãn mác từ nhà sản xuất.</p>
+            <p class="reciep__logo"><img src="/themes/v1/icons/logo-footer.svg" alt=""></p>
         </div>
         <div class="reciep__content">
         	<form action="" id="invoicePrint" method="POST">
-            <h2>CUSTOMER RECEIPT</h2>
             <p class="font-600 fs-14 mgB-5">DATE/TIME:{{date('Y-m-d H:i:s')}}</p>
             {{--<p class="font-600 fs-14 mgB-5">SERVED BY:</p>--}}
             <p class="font-600 fs-14 mgB-5">INVOICE #:<input type="text" class="input-label" name="invoice[invoice_number]" placeholder="INVOICE NUMBER"></p>
@@ -44,9 +40,10 @@
             <p class="font-600 fs-14 mgB-5">EMAIL: <input type="text" class="input-label" name="invoice[customer][email]" placeholder="EMAIL"></p>
             <p class="font-600 fs-14 mgB-5">PHONE:  <input type="text" class="input-label" name="invoice[customer][phone]" placeholder="PHONE"></p>
             <p class="font-600 fs-14 mgB-5">ADDRESS:  <input type="text" class="input-label" name="invoice[customer][address]" placeholder="ADDRESS"></p>
-            <p class="font-700 fs-14 text-center mgT-20 mgB-20">NOTICE!!!</p>
-            <p class="font-500 fs-13 font-600 text-center mgB-40">Sản phẩm chỉ được đổi trả trong vòng 03 ngày kể từ ngày mua hàng với điều kiện quý khách còn giữ hóa đơn và sản phẩm chưa qua sử dụng còn nguyên nhãn mác từ nhà sản xuất.</p>
-            <p class="font-700 fs-14 text-center mgT-20 mgB-20">THANK YOU AND HOPE YOU HAD GREAT SHOPPING EXPERIENCE</p>
+            <p class="font-700 fs-14 text-center mgT-20 mgB-10">QUY ĐỊNH ĐỔI TRẢ HÀNG</p>
+            <p class="font-500 fs-13 font-600 text-center mgB-40">Sản phẩm chỉ được đổi trả trong vòng 03 ngày kể từ ngày mua hàng với điều kiện quý khách còn giữ hóa đơn và sản phẩm chưa qua sử dụng còn nguyên nhãn mác từ nhà sản xuất - không áp dụng cho sản phẩm được giảm giá.</p>
+            <p class="font-700 fs-14 text-center mgT-20 mgB-10">CTY TNHH THƯƠNG MẠI HÙNG TÂM</p>
+            <p class="font-500 fs-13 font-600 text-center mgB-40">135/58 Trần Hưng Đạo , District 1, HCM city , Việt Nam .<br/> glab.vn@gmail.com  -   094 537 88 09  </p>
             <div class="text-right mgT-30">
                 <button class="btn btn-glab btn-default btn-sm btn-print"><i class="fa fa-print"></i>Print</button>
             </div>
